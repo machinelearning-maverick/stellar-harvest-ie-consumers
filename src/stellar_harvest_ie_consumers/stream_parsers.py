93321da63f61e1930a82eab44715800894be7c3c
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-05-16 21:48:35 +0100

    feat(stream_parsers): Enhance parse_planetary_kp_index function to return complete KP index data

--- src/stellar_harvest_ie_consumers/stream_parsers.py
+++ src/stellar_harvest_ie_consumers/stream_parsers.py
@@ -2,7 +2,15 @@ from datetime import datetime
 from stellar_harvest_ie_models.stellar.swpc.models import KpIndexRecord as KpIndexModel
 
 
-def parse_planetary_kp_index(kp_index: dict):
-    """ """
+def parse_planetary_kp_index(kp_index: dict) -> dict:
+    """
+    Take the JSON-dict from Kafka and produce a flat dict
+    with exactly the columns for your ORM model.
+    """
     record: KpIndexModel = KpIndexModel(**kp_index)
-    return {"time_tag": record.time_tag}
+    return {
+        "time_tag": record.time_tag,
+        "kp_index": record.kp_index,
+        "estimated_kp": record.estimated_kp,
+        "kp": record.kp,
+    }
